cebbc7c606ae0ed2ca4840baf22db8c26766ca99
Ilya Boyko <furagi.usagi@gmail.com>
2025-03-21 17:44:59 +0400

    added some graphql deps, removed redundant commented code

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -1,6 +1,5 @@
 import { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
-// import { fixupConfigRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import nx from '@nx/eslint-plugin';
